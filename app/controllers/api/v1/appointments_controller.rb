27c0312eda01bb20e710ce912a7581ec8ae3c571
Uloaku Enyingwa <uloshuwari@gmail.com>
2021-07-13 13:42:37 +0100

    feature(appointment): add appointment show, update and destroy endpoints

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -3,9 +3,11 @@
 module Api
   module V1
     class AppointmentsController < ApplicationController
-      before_action :set_mentor, only: %i[create]
-      around_action :setup_time_zone, only: :create
-      before_action :appointment_params, only: %i[create]
+      before_action :set_mentor, only: %i[create show update]
+      around_action :setup_time_zone, only: %i[create show update]
+      before_action :appointment_params, only: %i[create update]
+      before_action :set_appointment, only: %i[show update destroy]
+
 
       def create
         @appointment = @mentor.appointments.new(appointment_params)
@@ -24,10 +26,42 @@ module Api
         end
       end
 
+      def show
+        json_response(
+          @appointment,
+          ::V1::AppointmentSerializer,
+          SuccessMessage.appointment_show
+        )
+      end
+
+      def update
+        @appointment.attributes = appointment_params
+        @appointment.end_at = @appointment.begin_at.advance(hours: 1)
+        if @appointment.overlapping?
+          json_error_response(ErrorMessage.time_overlap)
+        elsif @appointment.save
+          json_response(
+            @appointment,
+            ::V1::AppointmentSerializer,
+            SuccessMessage.appointment_update
+          )
+        else
+          json_invalid_response(@appointment)
+        end
+      end
+
+      def destroy
+        render json: { status: true, message: SuccessMessage.appointment_destroy } if @appointment.destroy
+      end
+
       private
 
       def appointment_params
         params.require(:appointment).permit(:student_id, :description, :begin_at)
+      end
+
+      def set_appointment
+        @appointment = Appointment.find(params[:id])
       end
 
       def set_mentor
--- app/lib/success_message.rb
+++ app/lib/success_message.rb
@@ -4,4 +4,16 @@ class SuccessMessage
   def self.appointment_create
     'Appointment Created'
   end
+
+  def self.appointment_show
+    'Appointment Retrieved'
+  end
+
+  def self.appointment_update
+    'Appointment Updated'
+  end
+
+  def self.appointment_destroy
+    'Appointment Deleted'
+  end
 end
